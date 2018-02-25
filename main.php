1f37fd0b87ed3e2790389a7329365302cd035899
mnao305 <naomasa305@gmail.com>
2018-02-25 23:05:10 +0900

    [add]GoogleAnalytics導入

--- main.php
+++ main.php
@@ -33,6 +33,14 @@ try {
 <!DOCTYPE html>
 <html lang="ja">
 <head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-57438486-3"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'UA-57438486-3');
+	</script>
 	<meta charset="utf-8">
 	<title>imgFavtter</title>
 	<link rel="stylesheet" href="./css/style.css">
